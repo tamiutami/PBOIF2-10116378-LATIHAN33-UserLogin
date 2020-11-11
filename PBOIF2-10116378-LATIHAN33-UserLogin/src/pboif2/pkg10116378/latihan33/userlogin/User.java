495775a4fe8d09be661874afdb62c8f661049230
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-12 01:29:07 +0700

    Add files via upload

---
+++ PBOIF2-10116378-LATIHAN33-UserLogin/src/pboif2/pkg10116378/latihan33/userlogin/User.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10116378.latihan33.userlogin;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-IF2
+ * Nim   : 10116378
+ */
+public class User {
+
+    private static String username;
+    private static String password;
+    private boolean statusAkun;
+
+    private boolean cekAkun(String parUserName, String parPassword){
+        username = "RizkiAdam";
+        password = "terbaikselalu";
+        if (parPassword.equals(password) && parUserName.equals(username)) {
+            statusAkun = true;
+        } else {
+            statusAkun = false;
+        }
+        return statusAkun;
+    }
+
+    private void hasilLogin(boolean status, String parUserName){
+        if (status==true) {
+            System.out.println("\n******HALLO "+parUserName.toUpperCase()+"******");
+            System.out.println("Selamat Datang di Aplikasi ini");
+        }else {
+            System.out.println("\nOoops, Username atau Password anda salah");
+        }
+    }
+
+    public void pengecekkanLogin(String parUserName, String parPassword){
+        cekAkun(parUserName,parPassword);
+        hasilLogin(statusAkun,parUserName);
+    }
+    
+    
+}
---
+++ PBOIF2-10116378-LATIHAN33-UserLogin/src/pboif2/pkg10116378/latihan33/userlogin/UserLogin.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10116378.latihan33.userlogin;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-IF2
+ * Nim   : 10116378
+ */
+public class UserLogin {
+    private static String usName;
+    private static String passWord;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Masukkan Username = ");
+        usName = scn.next();
+
+        System.out.print("Masukkan Password = ");
+        passWord = scn.next();
+
+        User cek = new User();
+        cek.pengecekkanLogin(usName,passWord);
+    }
+}
